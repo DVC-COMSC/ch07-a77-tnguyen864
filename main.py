8acf9b02a888876aa450eef0ea92ab1273f74324
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 10:43:04 -0700

    7-7

--- main.py
+++ main.py
@@ -12,4 +12,11 @@ if len(num1) < len(num2):
     for i in range(x):
         num3.append(num2[i-1])
 
+if len(num1) > len(num2):
+    for i in range(len(num2)):
+        num3.append(num1[i])
+        num3.append(num2[i])
+    x = len(num1) - len(num2)
+    for i in range(x):
+        num3.append(num1[i-1])
 print(num3)
