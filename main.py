1d0ae16024d2f93f08a5357d9de974fb0017b99c
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 10:41:12 -0700

    Assignment 7-7

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
 num3 = []
-num3.append(num1[0])
-num3.append(num2[0])
-num3.append(num1[1])
-num3.append(num2[1])
-num3.append(num1[2])
-num3.append(num2[2])
-num3.append(num2[3])
+
+if len(num1) < len(num2):
+    for i in range(len(num1)):
+        num3.append(num1[i])
+        num3.append(num2[i])
+    x = len(num2) - len(num1)
+    for i in range(x):
+        num3.append(num2[i-1])
 
 print(num3)
