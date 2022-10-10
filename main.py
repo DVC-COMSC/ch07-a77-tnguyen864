68b7f9b14a42cc559dedef1c27fb7add5bc69e43
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 10:22:14 -0700

    Assignment 7-7

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-# ******************************
-# Make your Code
-# ******************************
+num3 = []
+num3.append(num1[0])
+num3.append(num2[0])
+num3.append(num1[1])
+num3.append(num2[1])
+num3.append(num1[2])
+num3.append(num2[2])
+num3.append(num2[3])
 
-# print (num3) 
+print(num3)
